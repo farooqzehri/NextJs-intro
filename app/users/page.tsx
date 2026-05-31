b30ee67d8c8a568c9a4e8fd03352eaecc61928b4
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-05-31 23:21:13 +0500

    user I

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,22 +1,10 @@
-import { log } from 'node:console'
 import React from 'react'
 
-const Users = async () => {
-    const data = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await data.json()
-    console.log(users);
-    
+function Users() {
   return (
-    <>
-    {users.map((user: any) => {
-        return(
-            <div key={user.id}>
-                <h2>{user.name}</h2>
-                <h3>{user.email}</h3>
-            </div>
-        )
-    })}
-    </>
+<>
+<h1>Hello World</h1>
+</>
   )
 }
 
