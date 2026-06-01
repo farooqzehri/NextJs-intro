75f0dbf7e1b078928aea1213e72939422082ec58
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-02 00:54:36 +0500

    functioned added

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,15 +4,20 @@ function Users() {
   const [email , setEmail] = useState('')
   const [password , setPassword] = useState('')
 
+  const callme = () =>{
+   alert(`Hello Your email is ${email} and `)
+
+  }
+
   return (
 <>
 <h1>Hello World</h1>
 <h2>Users Telll me </h2>
 <div>
-  <button>Click me</button>
+  <button onClick={callme}>Click me</button>
   <form>
-    <input type="Email" placeholder="Enter your email" value={email}/>
-    <input type="password" placeholder="Enter your password" value={password}/>
+    <input type="Email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+    <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)  }/>
      <button type="submit">Submit</button>
   </form>
 </div>
