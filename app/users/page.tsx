745eadef62f5d05a23d2c0386a60b4def429e6f8
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-01 23:37:44 +0500

    btn added

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -10,6 +10,7 @@ function Users() {
   <form>
     <input type="Email" placeholder="Enter your email"/>
     <input type="password" placeholder="Enter your password"/>
+     <button type="submit">Submit</button>
   </form>
 </div>
 </>
