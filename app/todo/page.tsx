669cba7a9003672f203af5a37f54cdfce65cea66
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-04 23:16:41 +0500

    MAking aTod

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 function Todo() {
+  const [input , setInput] = useState('')
+  const [todos , setTodos] = useState([])
+
+  const addTodo = () => {
+    todos.push(input)
+  }
   return (
     <>
     <h1>Hello Todo</h1>
 
-    <input type="text" />
+    <input type="text" placeholder='Enter Your Todo.' value={input} onChange={e => setInput(e.target.value)}/>
+    <button>Add Todo</button>
     </>
   )
 }
