d6c1b7d5ee19cab1b361bc6736e93090530f1eae
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-10 01:19:42 +0500

    key added

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,6 +5,7 @@ function Services() {
     <div>
         <h1>Our Services</h1>
         <h3>We offer a wide range of services to meet your needs.</h3>
+        <h3>Here are some of our key services:</h3>
     </div>
   )
 }
