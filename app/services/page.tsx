3299fa979b30dd2940cd4fd03f07fe0ea1f1b99f
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-06-10 01:20:12 +0500

    listing

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,6 +6,9 @@ function Services() {
         <h1>Our Services</h1>
         <h3>We offer a wide range of services to meet your needs.</h3>
         <h3>Here are some of our key services:</h3>
+        <ul>
+            <li></li>
+        </ul>
     </div>
   )
 }
